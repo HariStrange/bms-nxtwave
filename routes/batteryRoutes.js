c34c3af502ae9401c063c1894612540ecf25b2a2
Haristrange7 <haristrange1@gmail.com>
2025-11-04 16:25:24 +0530

    BMS final

--- routes/batteryRoutes.js
+++ routes/batteryRoutes.js
@@ -1,16 +1,21 @@
 const express = require("express");
 
-const batteryController = require("../controllers/batteryControllers");
+const {
+  postBatteryData,
+  getBatteryData,
+  getBatteryDataById,
+  getBatteryFieldData,
+} = require("../controllers/batteryControllers");
 const authenticate = require("../middleware/auth");
 
 const router = express.Router();
 
-router.post("/data", authenticate, batteryController.postBatteryData);
+router.post("/data", authenticate, postBatteryData);
 
-router.get("/all", authenticate, batteryController.getBatteryData);
+router.get("/all", authenticate, getBatteryData);
 
-router.get("/:id", authenticate, batteryController.getBatteryDataById);
+router.get("/:id", authenticate, getBatteryDataById);
 
-router.get("/:id/:field", authenticate, batteryController.getBatteryFieldData);
+router.get("/:id/:field", authenticate, getBatteryFieldData);
 
 module.exports = router;
